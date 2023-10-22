2cbd7e1d314e87a9e83dda7ac6f27db99a15edd8
Thanveertt <thanveer7tt@gmail.com>
2023-10-22 21:58:11 +0530

    Create Left Half pyramid pattern of stars.c

---
+++ Left Half pyramid pattern of stars.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,row;
+    printf("enter the number of rows:");
+    scanf("%d",&row);
+    for(i=1;i<=row;i++)
+    {
+        for(j=1;j<=row;j++)
+        {
+        if((i+j)<=row)
+        printf(" ");
+        else
+        printf("*");
+        }
+        printf("\n");
+    }
+}
